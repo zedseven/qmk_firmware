a4793c0f4c006c62cecc56167e64a100f1974e15
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2024-02-01 03:12:42 -0500

    Add a `flake.nix` for entering an environment with `qmk` installed.

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell rec {
+      packages = with pkgs; [
+        qmk
+      ];
+    };
+  };
+}
